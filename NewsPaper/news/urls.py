bd5fb427bf544ca18075fdd0b844ec2a94c80782
Dmitry <d3011ss@yandex.ru>
2022-05-03 19:19:01 +0500

    D6_5

---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import NewsList, PostDetail, SearchList, PostCreateView, PostUpdateView, PostDeleteView, UserUpdateView, \
+   CategoryList, add_subscribe
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+   path('', NewsList.as_view()),
+   path('<int:pk>/', PostDetail.as_view(), name='post'),
+   path('search/', SearchList.as_view()),
+   path('create/', PostCreateView.as_view(), name='post_create'),
+   path('create/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+   path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+   path('user/', UserUpdateView.as_view(), name='user_update'),
+   path('category/', CategoryList.as_view(), name='appointment_post'),
+   path('subscribe/<int:pk>', add_subscribe, name='subscribe'),
+
+]
